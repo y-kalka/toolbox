a05a307935cd023e7d170feaf527507cbbaacc8c
Yannic Kalka <yannic@ykalka.dev>
2024-11-27 13:48:22 +0100

    Fixes

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+/** @deprecated Use the import from "@y-kalka/toolbox/email" */
+export * from "./email.js";
--- tsup.config.js
+++ tsup.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
 	entry: {
+		index: "src/index.ts",
 		email: "src/email.ts",
 	},
 	format: ["cjs", "esm"],
