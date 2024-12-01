851d9ff91771aa007136631d335f19c5c281c9d6
Yannic Kalka <yannic@ykalka.dev>
2024-12-01 10:51:55 +0100

    Fix formating

--- src/async.test.ts
+++ src/async.test.ts
@@ -4,14 +4,17 @@ import { sleep } from "./async.js";
 describe("sleep", (test) => {
 	const TIME_MS = 10;
 
-	test("should resolve the promise after or on time", // { repeats: 1000 },
-	async () => {
-		const start = performance.now();
-		await sleep(TIME_MS);
-		const time = performance.now() - start;
+	test(
+		"should resolve the promise after or on time",
+		{ repeats: 1 },
+		async () => {
+			const start = performance.now();
+			await sleep(TIME_MS);
+			const time = performance.now() - start;
 
-		expect(time).gte(Math.floor(TIME_MS * 0.95));
-	});
+			expect(time).gte(Math.floor(TIME_MS * 0.95));
+		},
+	);
 
 	test("should return a promise", async () => {
 		expect(sleep(TIME_MS)).resolves.toBe(undefined);
