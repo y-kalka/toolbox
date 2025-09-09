732057f6e5ab580c93e0df72eb7f1c6b0a7501df
Yannic Kalka <yannic@ykalka.dev>
2025-09-09 13:06:03 +0200

    Remove index.js

--- src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "vitest";
-import { email } from "./index.js";
-
-test("should load sub packages", () => {
-	expect(email).toBeTypeOf("object");
-	expect(email.setSubAddress).toBeTypeOf("function");
-});
